250640928a3fd5f8a5957e7ec516a17f959c4056
mikholi <keranemi@icloud.com>
2024-09-09 10:20:10 +0300

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 
+function App() {
+    return (
+        <div>
+            <h1
+            onMouseEnter={()=> console.log("Hiiri tuli mestoille")}
+            onClick={()=> window.alert('Otsikkoa painettu')}
+
+            >Heippa</h1>
+        </div>
+    )
+}
 
 export default App
